b520795b36d7e0e0ab4b23432a248ba57de9388d
Ambrish-git <mycodinglibrary@gmail.com>
2019-06-28 07:44:08 +0530

    eslint and prettier setup with webpack

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Welcome = () => {
+  return <h1>Hello World from React + webpack+ prettier +eslint!!! </h1>;
+};
+
+ReactDOM.render(<Welcome />, document.getElementById('root'));
